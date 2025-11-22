bc41577741941eae6798753b096e88dfd84e5aad
MFajarTrimulyo <90810685+MFajarTrimulyo@users.noreply.github.com>
2025-11-22 22:09:15 +0700

    Off in seeder rekening

--- database/seeders/RekeningSeeder.php
+++ database/seeders/RekeningSeeder.php
@@ -13,28 +13,28 @@ class RekeningSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('rekenings')->insert([
-            [
-                'id' => 'REK001',
-                'nama_bank' => 'Bank BRI',
-                'nomor_rekening' => '1234567890123456',
-                'atas_nama' => 'YUM Official',
-                'is_active' => true,
-            ],
-            [
-                'id' => 'REK002',
-                'nama_bank' => 'Bank BCA',
-                'nomor_rekening' => '888012345678',
-                'atas_nama' => 'YUM Official',
-                'is_active' => true,
-            ],
-            [
-                'id' => 'REK003',
-                'nama_bank' => 'E-Wallet DANA',
-                'nomor_rekening' => '081234567890',
-                'atas_nama' => 'YUM Official',
-                'is_active' => true,
-            ],
-        ]);
+        // DB::table('rekenings')->insert([
+        //     [
+        //         'id' => 'REK001',
+        //         'nama_bank' => 'Bank BRI',
+        //         'nomor_rekening' => '1234567890123456',
+        //         'atas_nama' => 'YUM Official',
+        //         'is_active' => true,
+        //     ],
+        //     [
+        //         'id' => 'REK002',
+        //         'nama_bank' => 'Bank BCA',
+        //         'nomor_rekening' => '888012345678',
+        //         'atas_nama' => 'YUM Official',
+        //         'is_active' => true,
+        //     ],
+        //     [
+        //         'id' => 'REK003',
+        //         'nama_bank' => 'E-Wallet DANA',
+        //         'nomor_rekening' => '081234567890',
+        //         'atas_nama' => 'YUM Official',
+        //         'is_active' => true,
+        //     ],
+        // ]);
     }
 }
