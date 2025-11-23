45c48d5c8daca5e230ea7e0f1eb90fd5ecfa632e
MFajarTrimulyo <90810685+MFajarTrimulyo@users.noreply.github.com>
2025-11-23 14:16:00 +0700

    layout penjual laporan fix

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -125,13 +125,6 @@
                             </li>
 
                             @endif
-                            <!-- Laporan -->
-                            <li>
-                                <a href="{{ route('laporan.index', Auth::user()->role) }}" class="flex items-center px-4 py-3 text-gray-600 hover:bg-gray-50 hover:text-yum-primary rounded-lg font-medium transition-colors">
-                                    <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 002 2h2a2 2 0 002-2z"></path></svg>
-                                    Laporan
-                                </a>
-                            </li>
                         @endif
 
                         @if (Auth::user()->role == 'penjual')
@@ -176,6 +169,14 @@
 
                             <hr class="text-gray-300">
                         @endif
+
+                        <!-- Laporan -->
+                        <li>
+                            <a href="{{ route('laporan.index', Auth::user()->role) }}" class="flex items-center px-4 py-3 text-gray-600 hover:bg-gray-50 hover:text-yum-primary rounded-lg font-medium transition-colors">
+                                <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 002 2h2a2 2 0 002-2z"></path></svg>
+                                Laporan
+                            </a>
+                        </li>
                 </ul>
             </nav>
 
